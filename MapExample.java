b99b59a81ce9f8423afcb6246be492b217c8c8f3
sampurnapandit <101409704+sampurnapandit@users.noreply.github.com>
2023-12-29 17:12:20 +0530

    Update MapExample.java

--- MapExample.java
+++ MapExample.java
@@ -5,15 +5,19 @@ public class MapExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String sid[]= {"s1","s2","s3","s4","s5"};//Array containing student id
-		int marks[]= {78,89,70,56,63};//Array containing marks
+		int marks[]= {78,89,70,56,63};//Arrays containing marks
 		
 		HashMap h=new HashMap();
 		for(int i=0;i<sid.length;i++)//iterating from the Arrays
 		{
 			h.put(sid[i],marks[i]);
+			
 		}
-		System.out.println("elements of the hashmap:"+h);
+		h.forEach((key,value)->System.out.println("Student id:"+key+"  Marks:"+value));
 		System.out.println("size of the hashmap:"+h.size());
+		
+		
+		
 
 	}
 
